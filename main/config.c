db07fb8e139f0f92b0889c4a880d1f5a896f9de9
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-09 01:38:48 +0300

    config: read config file to char pointer

--- main/config.c
+++ main/config.c
@@ -11,13 +11,16 @@ static const char *TAG = "WILLOW/CONFIG";
 void config_print(void)
 {
     esp_log_level_set(TAG, ESP_LOG_DEBUG);
+    char *config = NULL;
 
     ESP_LOGI(TAG, "opening %s", CONFIG_PATH);
+
     FILE *f = fopen(CONFIG_PATH, "r");
     if (f == NULL) {
         ESP_LOGE(TAG, "failed to open %s", CONFIG_PATH);
         goto close;
     }
+
     struct stat fs;
     if (stat(CONFIG_PATH, &fs)) {
         ESP_LOGE(TAG, "failed to get file status");
@@ -25,11 +28,12 @@ void config_print(void)
     }
     ESP_LOGI(TAG, "config file size: %ld", fs.st_size);
 
-    char line[4096];
-    ESP_LOGI(TAG, "config file content:");
-    while (fgets(line, sizeof(line), f) != NULL) {
-        printf("%s\n", line);
-    };
+    config = calloc(sizeof(char), fs.st_size + 1);
+    size_t rlen = fread(config, 1, fs.st_size, f);
+    ESP_LOGI(TAG, "fread: %d", rlen);
+    config[fs.st_size] = '\0';
+    ESP_LOGI(TAG, "config file content: %s", config);
+    free(config);
 close:
     fclose(f);
 }
